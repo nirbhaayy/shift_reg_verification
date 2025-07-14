14e23f51bf010e46260e245e7e09e6766928b304
nirbhaythakkar <91128749+nirbhaayy@users.noreply.github.com>
2025-07-14 13:17:26 +0530

    Create sequencer.sv

---
+++ sequencer.sv
@@ -0,0 +1,9 @@
+class sequencer extends uvm_sequencer#(seq_item);
+  
+  `uvm_component_utils(sequencer)  
+
+  function new(string name="sequencer",uvm_component parent = null);  
+    super.new(name,parent);
+  endfunction
+  
+endclass : sequencer
